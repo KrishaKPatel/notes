993d545525c85c0fcd6780aacffe640fdb36ff49
Krisha Ketankumar Patel <patelkrisha114@gmail.com>
2025-02-10 14:57:04 -0500

    first-commit

---
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -0,0 +1,80 @@
+import { Link, useNavigate } from "react-router-dom"
+import Navbar from "../../components/Navbar/Navbar"
+import PasswordInput from "../../components/Input/PasswordInput"
+import { useState } from "react"
+import { validateEmail } from "../../utils/helper"
+import axiosInstance from "../../utils/axiosInstance"
+
+
+const Login = () => {
+
+  const [email,setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async(e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)){
+      setError("Please enter a valid email address.")
+      return;
+    }
+
+    if(!password){
+      setError("please enter the password.")
+      return;
+    }
+
+    setError("")
+
+    try{
+      const response = await axiosInstance.post("/login", {
+        email: email,
+        password: password,
+      });
+      if(response.data && response.data.accessToken){
+        localStorage.setItem("token", response.data.accessToken);
+        navigate('/dashboard');
+      }
+    }
+    catch (error){
+      if (error.response && error.response.data && error.response.data.message){
+        setError(error.response.data.message);
+      }
+      else{
+        setError("An unexpected error occurred. Please try again.");
+      }
+    }
+  };
+  return <>
+    <Navbar />
+    <div className="flex items-center justify-center mt-28">
+      <div className="w-96 border rounded bg-white px-7 py-16">
+        <form onSubmit={handleLogin}>
+          <h4 className="text-2xl mb-7">Login</h4>
+          <input type="text" placeholder="Email" className="input-box" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <PasswordInput value={password}
+          onChange={(e) => setPassword(e.target.value)}/>
+
+          {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+          <button className="btn-primary" type="submit">Login</button>
+
+          
+
+          <p className="text-sm text-center mt-4">Not Registered yet?{" "}
+            <Link to="/signup" className="font-medium text-primary underline"> Create an Account</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  </>
+  
+}
+
+export default Login
---
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,100 @@
+import { Link, useNavigate } from "react-router-dom";
+import PasswordInput from "../../components/Input/PasswordInput";
+import Navbar from "../../components/Navbar/Navbar";
+import { useState } from "react"
+import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
+
+
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if(!name){
+      setError("Please enter your name");
+      return;
+    }
+    if (!validateEmail(email)){
+        setError("Please enter a valid email address.")
+        return;
+      }
+  
+      if(!password){
+        setError("please enter the password.")
+        return;
+      }
+      setError("");
+
+      try{
+        const response = await axiosInstance.post("/create-account", {
+          fullName: name,
+          email: email,
+          password: password,
+        });
+
+        if(response.data && response.data.error){
+          setError(response.data.message)
+          return
+        }
+
+        if(response.data && response.data.accessToken){
+          localStorage.setItem("token", response.data.accessToken);
+          navigate('/dashboard');
+        }
+      }
+      catch (error){
+        if (error.response && error.response.data && error.response.data.message){
+          setError(error.response.data.message);
+        }
+        else{
+          setError("An unexpected error occurred. Please try again.");
+        }
+      }
+  }
+
+  return (
+    <>
+    <Navbar />
+    <div className="flex items-center justify-center mt-28">
+      <div className="w-96 border rounded bg-white px-7 py-16">
+        <form onSubmit={handleSignUp}>
+          <h4 className="text-2xl mb-7">Sign Up</h4>
+
+          <input type="text" 
+          placeholder="Name" 
+          className="input-box" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          />
+          <input type="text" 
+          placeholder="Email" 
+          className="input-box" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          />
+
+        <PasswordInput value={password}
+          onChange={(e) => setPassword(e.target.value)}/>
+
+        {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+          <button className="btn-primary" type="submit">SignUp</button>
+
+          <p className="text-sm text-center mt-4">Already have an account?{" "}
+            <Link to="/login" className="font-medium text-primary underline"> Login</Link>
+          </p>
+          </form>
+        </div>  
+      </div>
+    </>
+    
+  );
+};
+
+export default SignUp
